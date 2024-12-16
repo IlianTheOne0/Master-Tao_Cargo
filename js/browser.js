cedc12dc64b18e6e4383e91b51aeba1028418c1b
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-16 17:18:44 +0200

    Temp

--- js/browser.js
+++ js/browser.js
@@ -1,5 +1,5 @@
-var is_ios = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
-if(is_ios)    
-{
-     $('<link rel="stylesheet" type="text/css" href="../css/ios.css">').appendTo("head");
-};
+// var is_ios = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
+// if(is_ios)    
+// {
+//      $('<link rel="stylesheet" type="text/css" href="../css/ios.css">').appendTo("head");
+// };
