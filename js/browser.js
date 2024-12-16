d54df7acb047f5137c30b014f5aebca36488d49c
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-16 17:29:15 +0200

    Completed

--- js/browser.js
+++ js/browser.js
@@ -1,5 +1,5 @@
 var is_ios = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
 if(is_ios)    
 {
-     $('<link rel="stylesheet" type="text/css" href="../css/ios.css">').appendTo("head");
+     $('<link rel="stylesheet" type="text/css" href="https://iliantheone0.github.io/Master-Tao_Cargo/css/ios.css">').appendTo("head");
 };
