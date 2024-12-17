ae3a17958bdf5c43ddab9bb372c7f44c292b61be
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-17 16:35:32 +0200

    Completed

--- js/elements.js
+++ js/elements.js
@@ -2,3 +2,28 @@ document.getElementById('file-input').addEventListener('change', function(event)
     const fileName = event.target.files[0] ? event.target.files[0].name : '';
     document.querySelector('.file-item p').textContent = fileName;
 });
+
+
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function copyToClipboard(element) {
+    const text = element.textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Copied to clipboard: ' + text);
+    }).catch(err => {
+        console.error('Failed to copy: ', err);
+    });
+}
